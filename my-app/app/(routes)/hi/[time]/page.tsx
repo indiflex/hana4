52c9cb02106cc8347786614f0e68039b8d83a8be
indiflex <indiflex1@gmail.com>
2024-10-08 13:37:11 +0900

    nest dynamic routes

--- my-app/app/(routes)/hi/[time]/page.tsx
+++ my-app/app/(routes)/hi/[time]/page.tsx
@@ -2,12 +2,22 @@ type Params = {
   params: {
     time: string;
   };
+  searchParams: {
+    q: string;
+  };
 };
 
 export function generateStaticParams() {
   return ['morning', 'afternoon', 'evening', 'night'].map((time) => ({ time }));
 }
 
-export default function Time({ params: { time } }: Params) {
-  return <div className='capitalize'>Good {time}~</div>;
+export default function Time({
+  params: { time },
+  searchParams: { q },
+}: Params) {
+  return (
+    <div className='capitalize'>
+      Good {time}! <span className='text-red-500 font-bold'>{q}</span>
+    </div>
+  );
 }
--- my-app/components/Nav.tsx
+++ my-app/components/Nav.tsx
@@ -4,6 +4,7 @@ export default function Nav() {
       <a href='/'>Home</a>
       <a href='/hello'>Hello</a>
       <a href='/hi'>Hi</a>
+      <a href='/shop/aaa'>Shop</a>
       <a href='/about'>About</a>
     </nav>
   );
