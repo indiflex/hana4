73eca276b6bb49724d23715cf3585d9d42a01218
indiflex <indiflex1@gmail.com>
2024-10-15 08:03:32 +0900

    strictEqual => eaual

--- js/utils/hangul-utils.js
+++ js/utils/hangul-utils.js
@@ -27,15 +27,15 @@ const eyuya = str => josa(str, '이어야/여야');
 const rang = str => josa(str, '이랑/랑');
 
 function testing() {
-  assert.strictEqual(`고성군${iga('고성군')}`, '고성군이');
-  assert.strictEqual(`고성군${eunun('고성군')}`, '고성군은');
-  assert.strictEqual(`고성군${eulul('고성군')}`, '고성군을');
-  assert.strictEqual(`성동구${iga('성동구')}`, '성동구가');
-  assert.strictEqual(`성동구${eunun('성동구')}`, '성동구는');
-  assert.strictEqual(`성동구${eulul('성동구')}`, '성동구를');
+  assert.equal(`고성군${iga('고성군')}`, '고성군이');
+  assert.equal(`고성군${eunun('고성군')}`, '고성군은');
+  assert.equal(`고성군${eulul('고성군')}`, '고성군을');
+  assert.equal(`성동구${iga('성동구')}`, '성동구가');
+  assert.equal(`성동구${eunun('성동구')}`, '성동구는');
+  assert.equal(`성동구${eulul('성동구')}`, '성동구를');
 
-  assert.strictEqual(`고성군${eyuya('고성군')}`, '고성군이어야');
-  assert.strictEqual(`성동구${eyuya('성동구')}`, '성동구여야');
+  assert.equal(`고성군${eyuya('고성군')}`, '고성군이어야');
+  assert.equal(`성동구${eyuya('성동구')}`, '성동구여야');
 }
 // testing();
 
