bed4c36ff78d15684bb46b8e267ba320a05574d0
indiflex <indiflex1@gmail.com>
2024-11-15 14:47:54 +0900

    1.1

--- sql/sqls/ddl.sql
+++ sql/sqls/ddl.sql
@@ -1,7 +1,7 @@
 create table Student (
   id mediumint unsigned not null auto_increment comment '학번',
   createdate timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '등록일시',
-  updatedate timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '등록일시',
+  updatedate timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
   name varchar(31) not null comment '이름',
   birthdt varchar(6) not null comment '생일(YYMMDD)',
   major tinyint unsigned not null comment '학과코드',
@@ -25,4 +25,42 @@ desc Student;
 create table Major (
   id tinyint unsigned not null auto_increment primary key comment '학과코드',
   name varchar(20) not null comment '학과명'
+);
+
+create table Prof (
+  id smallint unsigned not null auto_increment comment '교수번호',
+  name varchar(31) not null comment '교수명',
+  likecnt int default 0,
+  Primary Key (id)
+);
+
+-- modify name var..
+-- change name namex var.......
+
+alter table Prof add column createdate timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '등록일시' after id;
+alter table Prof add column updatedate timestamp DEFAULT CURRENT_TIMESTAMP
+                 ON UPDATE CURRENT_TIMESTAMP COMMENT '등록일시' after createdate;
+                 
+desc Prof;
+
+create table Subject (
+  id smallint unsigned not null auto_increment comment '과목번호',
+  createdate timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '등록일시',
+  updatedate timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
+  name varchar(45) not null comment '과목명',
+  prof smallint unsigned null comment '담당교수',
+  Primary Key (id),
+  Constraint Foreign Key fk_Subject_prof_Prof (prof)
+                 References Prof(id) on Delete set null on Update cascade
+);
+
+alter table Subject add constraint unique key uniq_Subject_name(name);
+
+show index from Subject;
+
+create table Enroll (
+  id int unsigned not null auto_increment comment '수강신청번호',
+  createdate timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '신청일시',
+  updatedate timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
+  
 );
